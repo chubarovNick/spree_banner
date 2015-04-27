e9eaedb823008ff034cb694f349846e0493f1793
Nick Chubarov <nick.chubarov@gmail.com>
2015-04-27 16:23:52 +0400

    Disable banner boxes for admin navigation bar

--- app/overrides/banner_admin_tab.rb
+++ app/overrides/banner_admin_tab.rb
@@ -1,9 +1,3 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
-                      :name => "banner_box_admin_tab",
-                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<%= tab(:banner_boxes, :icon => 'icon-bookmark') %>",
-                      :disabled => false)
-
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                       :name => "add_banner_box_settings",
                       :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
